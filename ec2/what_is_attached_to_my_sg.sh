78636a5b1f494a73d65fbf0f71fd36a56813db1a
ido123ziv <idoshziv@gmail.com>
2022-01-09 18:44:56 +0200

    added profile usage

--- ec2/what_is_attached_to_my_sg.sh
+++ ec2/what_is_attached_to_my_sg.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 # $1 is a security group to check, default is the default sg
 # $2 is a region to check, default is the default eu-west-1
-SECURITY_GROUP=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=default")
 if [[ ! -z $1 ]]; then
-    SECURITY_GROUP=$1
-REGION="eu-west-1"
+    PROFILE=$1
+fi
+SECURITY_GROUP=$(aws ec2 describe-security-groups --profile $PROFILE --filters "Name=group-name,Values=default")
 if [[ ! -z $2 ]]; then
-    REGION=$2
-aws ec2 describe-network-interfaces --filters Name=group-id,Values=$SECURITY_GROUP --region $REGION --output json  --query 'NetworkInterfaces[*]'.['NetworkInterfaceId','Description','PrivateIpAddress','VpcId']
+    SECURITY_GROUP=$2
+fi
+REGION="eu-west-1"
+if [[ ! -z $3 ]]; then
+    REGION=$3
+fi
+aws ec2 describe-security-groups --query "SecurityGroups[].{Name:GroupName,Id:GroupId}" --region $REGION --profile $PROFILE | jq -c  " .[] " | while read line
+do
+    NAME=$(echo $line | jq ' .Name ')
+    SECURITY_GROUP=$(echo $line | jq ' .Id ')
+    echo SG is: $NAME
+    aws ec2 describe-network-interfaces --filters Name=group-id,Values=$SECURITY_GROUP --region $REGION --profile $PROFILE --output json  --query "NetworkInterfaces[*].[NetworkInterfaceId,Description,PrivateIpAddress,VpcId]"
+done
--- s3/get_bucket_encryption.sh
+++ s3/get_bucket_encryption.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-aws s3 ls | awk '{print $3}' | while read line ; do echo $line; aws s3api get-bucket-encryption --bucket $line; done
+if [[ ! -z $1 ]]; then
+    PROFILE=$1
+fi
+aws s3 ls --profile $PROFILE | awk '{print $3}' | while read line ; do echo $line; aws s3api get-bucket-encryption --bucket $line --profile $PROFILE; done
